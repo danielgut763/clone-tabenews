0b6abe71614766518a3510df06c367cc526fe958
danielgut763 <79209490+danielgut763@users.noreply.github.com>
2026-08-06 17:14:15 +0000

    a lot

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,18 @@
+import database from "infra/database.js";
+
+beforeAll(cleanupDatabase);
+
+async function cleanupDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
   
   const responseBody = await response.json();
+
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
