0685e00a1be2243db8ba98d5f0d254d1fb6bd526
danielgut763 <79209490+danielgut763@users.noreply.github.com>
2026-08-18 14:54:57 +0000

    end point

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,12 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
   const defaultMigrationsOptions = {
+    dbClient: dbClient,
     databaseUrl: process.env.DATABASE_URL,
     dryRun: true,
     dir: join("infra", "migrations"),
@@ -13,8 +17,8 @@ export default async function migrations(request, response) {
 
   if(request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-
-  return response.status(200).json(pendingMigrations);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
   
   if(request.method === "POST"){
@@ -22,6 +26,8 @@ export default async function migrations(request, response) {
       ...defaultMigrationsOptions,
       dryRun: false,
     });
+    
+    await dbClient.end();
 
   if (migratedMigrations.length > 0) {
     return response.status(201).json(migratedMigrations);
